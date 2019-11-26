74a8e9e8ac610e143b0ddef2ef5246f54bc00d27
uri121 <robinov121@gmail.com>
2019-11-26 12:00:36 +0200

    Updates

---
+++ precache-manifest.38ba2cfc87a0f6d7c343f3f8d3af830d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d8d3c1166ec3d7bf8474b4f67a2e08",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e90582be8dc5c496c2e8",
+    "url": "/portfolio/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "13be423c24aae0923220",
+    "url": "/portfolio/static/css/main.e3e9a0a9.chunk.css"
+  },
+  {
+    "revision": "e90582be8dc5c496c2e8",
+    "url": "/portfolio/static/js/2.c919e79d.chunk.js"
+  },
+  {
+    "revision": "13be423c24aae0923220",
+    "url": "/portfolio/static/js/main.3d7c1b8e.chunk.js"
+  },
+  {
+    "revision": "17fd855b9039c0118eb0",
+    "url": "/portfolio/static/js/runtime-main.ac7687e7.js"
+  },
+  {
+    "revision": "567a430033d5dde6e2e6e4e70b6d57ab",
+    "url": "/portfolio/static/media/about-me.567a4300.jpg"
+  },
+  {
+    "revision": "d22ee3727a7216019c3848df6eafa024",
+    "url": "/portfolio/static/media/github.d22ee372.png"
+  },
+  {
+    "revision": "de7ca97bd1780f7dd5371d43d06934e1",
+    "url": "/portfolio/static/media/logo.de7ca97b.jpeg"
+  },
+  {
+    "revision": "dfcff5f7905882a202a5a3da932daf04",
+    "url": "/portfolio/static/media/todo-list.dfcff5f7.png"
+  },
+  {
+    "revision": "7ec5c3d9c59089586bdd6be12b68fbd7",
+    "url": "/portfolio/static/media/weather-app.7ec5c3d9.png"
+  }
+]);
