0aa88ccba654ceef2a661d8cee8343eb8ad8dd9d
uri121 <robinov121@gmail.com>
2020-07-31 11:07:55 +0300

    fixing caption responsivnse

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -1,31 +1,74 @@
 import React from "react";
 import "./SideDrawer.css";
-import { Link  } from "react-scroll";
+import { Link } from "react-scroll";
 
-const sideDrawer = props => {
-  let drawerClasses = 'side-drawer';
-  if(props.show){
-    drawerClasses = 'side-drawer open'
+const sideDrawer = (props) => {
+  let drawerClasses = "side-drawer";
+  if (props.show) {
+    drawerClasses = "side-drawer open";
   }
-  return(
-  <nav className={drawerClasses}>
-  <ul>
-          <li>
-            <Link id="link" activeClass="fire" to="landing-page" onClick={props.click} spy={true} smooth={true} offset={0} duration={1000}>Home</Link>
-          </li>
-          <li>
-          <Link id="link" activeClass="fire" to="about-me" onClick={props.click} spy={true} smooth={true} offset={0} duration={1000}>About</Link>
-          </li>
-          <li>
-          <Link id="link" activeClass="fire" to="projects" onClick={props.click} spy={true} smooth={true} offset={0} duration={1000}>Projects</Link>
-          </li>
-          <li>
-          <Link id="link" activeClass="fire" to="my-form-container" onClick={props.click} spy={true} smooth={true} offset={0} duration={1000}>Contact</Link>
-          </li>
-        </ul>
-  </nav>
+  return (
+    <nav className={drawerClasses}>
+      <ul>
+        <li>
+          <Link
+            id="link"
+            activeClass="fire"
+            to="landing-page"
+            onClick={props.click}
+            spy={true}
+            smooth={true}
+            offset={-60}
+            duration={1000}
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            id="link"
+            activeClass="fire"
+            to="about"
+            onClick={props.click}
+            spy={true}
+            smooth={true}
+            offset={-60}
+            duration={1000}
+          >
+            About
+          </Link>
+        </li>
+        <li>
+          <Link
+            id="link"
+            activeClass="fire"
+            to="project"
+            onClick={props.click}
+            spy={true}
+            smooth={true}
+            offset={-60}
+            duration={1000}
+          >
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link
+            id="link"
+            activeClass="fire"
+            to="contact"
+            onClick={props.click}
+            spy={true}
+            smooth={true}
+            offset={-60}
+            duration={1000}
+          >
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
-
 
 export default sideDrawer;
